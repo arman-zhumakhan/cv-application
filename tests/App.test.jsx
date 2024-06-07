9f6c929765c6ebe840a46bf83891852ad61db29e
Arman Zhumakhan <arman310501@gmail.com>
2024-06-07 15:07:32 +0800

    Add tests

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,10 +1,20 @@
-
+/* eslint-disable no-undef */
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../src/App";
 
 describe("App component", () => {
-  it("renders correct heading", () => {
-    render(<App />);
-    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+    it("renders correct heading", () => {
+      render(<App />);
+      expect(screen.getByText('CV Application')).toBeInTheDocument();
+    });
+
+    it('renders the sample data after clicking the Load Sample CV button', async () => { // Add 'async' keyword here
+      const user = userEvent.setup();
+      render(<App />);
+      const loadSampleCVButton = screen.getByText('Load Sample CV');
+      await user.click(loadSampleCVButton);
+      
+      expect(screen.getByText('Arman Zhumakhan')).toBeInTheDocument();
+    });
   });
-});
